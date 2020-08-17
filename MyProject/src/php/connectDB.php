ce8ca43960535a1b731b404473b3b2b82399a460
TienNghe <nghequyettien@gmail.com>
2020-08-18 00:32:35 +0700

    update 17/08/2020 ver5

--- MyProject/src/php/connectDB.php
+++ MyProject/src/php/connectDB.php
@@ -10,39 +10,7 @@
 //                            REG_DATE TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 //                            ISACTIVE INT NOT NULL)
 //        ";
-//        if($connet->query($sql)){
-//            echo "successfully";
-//        } else {
-//            echo "Failded"."<br>". $connet->error;
-//        }
-//    $severname = "localhost";
-//    $username = "root";
-//    $password = "";
-//    $dbname = "myDataBase";
-//    $connet = new mysqli($severname,$username,$password,$dbname);
-//    $sql = "SELECT * FROM USER";
-//    $result = $connet->query($sql);
-//
-//    if($result->num_rows > 0){
-//        echo "Data consits of ".$result->num_rows." lines";
-//        while($row = $result->fetch_assoc()){
-//            $data = "<br>";
-//            $data .= "TOKEN:".$row['TOKEN']."--";
-//            $data .= "ID:".$row['ID']."--";
-//            $data .= "FIRSTNAME: ".$row['FIRSTNAME']."--";
-//            $data .= "LASTNAME: ".$row['LASTNAME']."--";
-//            $data .= "USERNAME: ".$row['USERNAME']."--";
-//            $data .= "EMAIL: ".$row['EMAIL']."--";
-//            $data .= "PASSWORD: ".$row['PASSWORD']."--";
-//            $data .= "REG_DATE: ".$row['REG_DATE']."<br>";
-//            $data .= "ISACTIVE: ".$row['ISACTIVE']."<br>";
-//            echo $data;
-//        }
-//    }else{
-//        echo ($result->num_rows)."results";
-//    }
-//
-//    $connet->close();
+
     function connetDataBase(){
         $severname = "localhost";
         $username = "root";
