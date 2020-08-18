576e35ae88248788d77b3dd38bc010d51aa7d56a
TienNghe <nghequyettien@gmail.com>
2020-08-18 14:22:40 +0700

    change forgot password

--- MyProject/src/global/connectDB.php
+++ MyProject/src/global/connectDB.php
@@ -1,17 +1,4 @@
 <?php
-//        $sql = "
-//        CREATE TABLE USER( TOKEN CHAR(150) NOT NULL,
-//                            RESETPWD CHAR(150) NOT NULL,
-//                            CHECKRESET INT NOT NULL,
-//                            ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-//                            FIRSTNAME VARCHAR(30) NOT NULL,
-//                            LASTNAME VARCHAR(30) NOT NULL,
-//                            USERNAME VARCHAR(30) NOT NULL,
-//                            EMAIL VARCHAR(50) NOT NULL,
-//                            PASSWORD VARCHAR(150) NOT NULL,
-//                            REG_DATE TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-//                            ISACTIVE INT NOT NULL)
-//        ";
     function connetDataBase(){
         $severname = "localhost";
         $username = "root";
